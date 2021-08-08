a4c662ef5172d9846e96b39cba01f11a8ed38d2f
Resistor52 <ken@kennethghartman.com>
2021-08-08 18:34:52 -0400

    add ssh priv key vars2

--- gcp/output.tf
+++ gcp/output.tf
@@ -3,5 +3,5 @@ output "ip" {
 }
 
 output "ssh_connection_string" {
-  value = "ssh -i ${var.ssh_priv_key_file} ${var.gce_ssh_user}@${aws_instance.vm_instance.public_ip}"
+  value = "ssh -i ${var.ssh_priv_key_file} ${var.gce_ssh_user}@${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
 }
