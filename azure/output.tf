b4a53049658198843589a97bf338044f2cc780d1
Resistor52 <ken@kennethghartman.com>
2021-08-08 21:17:06 -0400

    change LF

--- azure/output.tf
+++ azure/output.tf
@@ -1,7 +1,7 @@
 output "ip" {
-  value = azurerm_public_ip.hitc.ip_address
+  value = data.azurerm_public_ip.vm.ip_address
 }
 
 output "ssh_connection_string" {
-  value = "ssh -i ${var.ssh_priv_key_file} ${var.ssh_user}@${azurerm_public_ip.hitc.ip_address}"
+  value = "ssh -i ${var.ssh_priv_key_file} ${var.ssh_user}@${data.azurerm_public_ip.vm.ip_address}"
 }
