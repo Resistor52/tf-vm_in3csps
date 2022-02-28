59dcfb8e5e399b8b1d7bededae8ea6e2c538905a
Kenneth G. Hartman <ken@kennethghartman.com>
2022-02-28 07:11:00 -0500

    fix depricated argument

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  region                  = var.aws_region
-  shared_credentials_file = var.aws_creds_file
-  profile                 = var.aws_profile
+  region                    = var.aws_region
+  shared_credentials_files  = var.aws_creds_file
+  profile                   = var.aws_profile
 }
 
 data "http" "myip" {
