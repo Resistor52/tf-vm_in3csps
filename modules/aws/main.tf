fe7d057d22fc65bdbeeb1f3e0693ab6dc38d5be5
Kenneth G. Hartman <ken@kennethghartman.com>
2022-02-28 07:23:58 -0500

    fix depricated argument2

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region                    = var.aws_region
-  shared_credentials_files  = var.aws_creds_file
+  shared_credentials_files  = [var.aws_creds_file]
   profile                   = var.aws_profile
 }
 
